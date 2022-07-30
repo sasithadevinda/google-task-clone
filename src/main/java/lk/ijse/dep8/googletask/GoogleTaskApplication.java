df059e99e1108e4e27d8446bde427a9b9c1de1fc
sasithadevinda <sasithadevindawasala@gmail.com>
2022-07-31 01:04:34 +0530

    Initialize the Project

---
+++ src/main/java/lk/ijse/dep8/googletask/GoogleTaskApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep8.googletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/dep8/googletask/GoogleTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep8.googletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
